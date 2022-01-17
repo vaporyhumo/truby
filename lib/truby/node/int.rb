cc4438179e3d89e3c96c9fc3b30724271e88e850
vaporyhumo <roanvilina@gmail.com>
2022-01-17 17:03:59 -0300

    Add `Truby::Node::Int`

---
+++ lib/truby/node/int.rb
@@ -0,0 +1,24 @@
+module Truby
+  class Node
+    class Int < self
+      def initialize _type = nil, value
+        @type = :int
+        @value = value
+      end
+
+      attr_reader :value
+
+      def == other
+        Boolean other.instance_of?(self.class) && other.value == value
+      end
+
+      def unparse
+        value.to_s
+      end
+
+      def transpile
+        value.to_s
+      end
+    end
+  end
+end
