159c56d3ae471fd26cd6e864b2df839a5432f35c
vaporyhumo <roanvilina@gmail.com>
2022-01-20 14:31:55 -0300

    Refactor `Truby::Token`

--- lib/truby/token.rb
+++ lib/truby/token.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 module Truby
-  Token = DataType.new type: Symbol, value: String
+  # Token = DataType.new type: Symbol, value: String
+  class Token
+    extend DataType
+
+    define_type type: Symbol, value: String
+  end
 end
--- spec/truby/token_spec.rb
+++ spec/truby/token_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 describe Truby::Token do
+  specify { expect(described_class.__type).to match(type: Symbol, value: String) }
 end
