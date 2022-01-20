69d217b30b68c1f1a659c1e5b189f0465d799e6c
vaporyhumo <roanvilina@gmail.com>
2022-01-20 14:46:07 -0300

    Add specs for `Truby::TokenFactory`

--- spec/truby/token_factory_spec.rb
+++ spec/truby/token_factory_spec.rb
@@ -1,29 +1,51 @@
 # frozen_string_literal: true
 
 describe Truby::TokenFactory do
+  def assert_equal actual, expected
+    expect(actual).to eq expected
+  end
+
   describe '.for' do
     context 'when given "nil"' do
-      specify { expect(described_class.for('nil')).to eq(Truby::Token.new(:nil, 'nil')) }
+      let(:actual) { described_class.for('nil') }
+      let(:expected) { Truby::Token.new(:nil, Truby::Config::NIL) }
+
+      specify { assert_equal actual, expected }
     end
 
     context 'when given "true"' do
-      specify { expect(described_class.for('true')).to eq(Truby::Token.new(:true, 'true')) }
+      let(:actual) { described_class.for('true') }
+      let(:expected) { Truby::Token.new(:true, Truby::Config::TRUE) }
+
+      specify { assert_equal actual, expected }
     end
 
     context 'when given "false"' do
-      specify { expect(described_class.for('false')).to eq(Truby::Token.new(:false, 'false')) }
+      let(:actual) { described_class.for('false') }
+      let(:expected) { Truby::Token.new(:false, Truby::Config::FALSE) }
+
+      specify { assert_equal actual, expected }
     end
   end
 
   describe '.false' do
-    specify { expect(described_class.false).to eq(Truby::Token.new(:false, 'false')) }
+    let(:actual) { described_class.false }
+    let(:expected) { Truby::Token.new(:false, Truby::Config::FALSE) }
+
+    specify { assert_equal actual, expected }
   end
 
   describe '.nil' do
-    specify { expect(described_class.nil).to eq(Truby::Token.new(:nil, 'nil')) }
+    let(:actual) { described_class.nil }
+    let(:expected) { Truby::Token.new(:nil, Truby::Config::NIL) }
+
+    specify { assert_equal actual, expected }
   end
 
   describe '.true' do
-    specify { expect(described_class.true).to eq(Truby::Token.new(:true, 'true')) }
+    let(:actual) { described_class.true }
+    let(:expected) { Truby::Token.new(:true, Truby::Config::TRUE) }
+
+    specify { assert_equal actual, expected }
   end
 end
