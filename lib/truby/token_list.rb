61504312f22858808e3fd0d207c403aafb6fc13f
vaporyhumo <roanvilina@gmail.com>
2022-01-21 18:35:06 -0300

    Refactor `Truby::TokenList#add`

--- lib/truby/token_list.rb
+++ lib/truby/token_list.rb
@@ -15,7 +15,14 @@ module Truby
     end
 
     def add char
-      TokenList::new Array TokenFactory::for last_lexeme_with char
+      TokenFactory::for(last_lexeme_with char).then do |maybe_token|
+        case maybe_token
+        when Token
+          TokenList::new [*tokens[0..-2], maybe_token]
+        else
+          TokenList::new [*tokens, TokenFactory::for(char)]
+        end
+      end
     end
 
     def last_lexeme_with char
