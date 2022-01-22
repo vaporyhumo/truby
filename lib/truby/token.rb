713fcc3839bce265fd6cf6f88203d44b68300d9b
vaporyhumo <roanvilina@gmail.com>
2022-01-22 00:30:14 -0300

    Bump `Truby::Token` to `typed: strict`

--- lib/truby/token.rb
+++ lib/truby/token.rb
@@ -1,10 +1,22 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module Truby
   class Token
-    extend Truby::DataType
+    extend Equality
 
-    define_type type: Symbol, value: String
+    define_equality :type, :value
+
+    sig { params(type: Symbol, value: String).void }
+    def initialize(type, value)
+      @type = T.let(TokenType::deserialize(type.to_s), TokenType)
+      @value = value
+    end
+
+    sig { returns TokenType }
+    attr_reader :type
+
+    sig { returns String }
+    attr_reader :value
   end
 end
