354284ff4dd1bccc165d777213c93180b2c62658
vaporyhumo <roanvilina@gmail.com>
2022-01-24 13:42:54 -0300

    Refactor a parser spec

--- spec/truby/parser_spec.rb
+++ spec/truby/parser_spec.rb
@@ -1,29 +1,40 @@
+# typed: false
 # frozen_string_literal: true
 
 describe Truby::Parser do
   describe '.call' do
+    let(:actual) { described_class.call(input) }
+
     context 'when given "false"' do
-      specify { expect(described_class.('false')).to match(Truby::Node::false) }
+      specify { expect(described_class.('false')).to eq(Truby::Node::false) }
     end
 
     context 'when given "nil"' do
-      specify { expect(described_class.('nil')).to   match(Truby::Node::nil) }
+      specify { expect(described_class.('nil')).to   eq(Truby::Node::nil) }
     end
 
     context 'when given "true"' do
-      specify { expect(described_class.('true')).to  match(Truby::Node::true) }
+      specify { expect(described_class.('true')).to  eq(Truby::Node::true) }
     end
 
     context 'when given "1"' do
-      specify { expect(described_class.('1')).to     match(Truby::Node::int('1')) }
+      specify { expect(described_class.('1')).to     eq(Truby::Node::int('1')) }
     end
 
     context 'when given "-1"' do
-      specify { expect(described_class.('-1')).to    match(Truby::Node::int('-1')) }
+      specify { expect(described_class.('-1')).to    eq(Truby::Node::int('-1')) }
     end
 
     context 'when given "foo=true"' do
-      specify { expect(described_class.('foo=true')).to match(Truby::Node::assign('foo', Truby::Node::true)) }
+      let(:input) { 'foo=true' }
+      specify { expect(actual).to eq(Truby::Node::lvar_assign('foo', Truby::Node::true, [])) }
+    end
+
+    context 'when given "Boolean foo=true"' do
+      let(:input) { 'Boolean foo=true' }
+      let(:expected) { Truby::Node::lvar_assign('foo', Truby::Node::true, []) }
+
+      specify { expect(actual).to eq expected }
     end
   end
 end
