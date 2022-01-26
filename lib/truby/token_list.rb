4b32cbc18aa6ccaf56cbca1eea6d4f2fbd12e4b9
vaporyhumo <roanvilina@gmail.com>
2022-01-26 03:57:50 -0300

    Refactor `Truby::TokenList`

--- lib/truby/token_list.rb
+++ lib/truby/token_list.rb
@@ -1,38 +1,58 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 
 module Truby
   class TokenList
-    extend Truby::DataType
-
-    define_type tokens: Array
-
     sig { returns TokenList }
     def self.empty
       new Array::new
     end
 
+    sig { params(tokens: T::Array[Token]).void }
+    def initialize tokens
+      @tokens = tokens
+      freeze
+    end
+
+    sig { returns T::Array[Token] }
+    attr_reader :tokens
+
     sig { params(other: TokenList).returns T::Boolean }
     def == other
-      @tokens.zip(other.tokens).all? { _1 == _2 }
+      tokens.size.equal?(other.tokens.size) && tokens.zip(other.tokens).all? { _1 == _2 }
     end
 
     sig { returns NilToken }
     def last
-      @tokens.last
+      tokens.last
     end
 
     sig { params(char: String).returns TokenList }
     def add char
-      TokenFactory::for(last_lexeme_with char).then do |maybe_token|
+      maybe_token_for_last_lexeme_with(char).then do |maybe_token|
         case maybe_token
-        when Token
-          TokenList::new [*tokens[0..-2], maybe_token]
-        else
-          TokenList::new [*tokens, TokenFactory::for(char)]
+        when Token then new_token_list_with_last_token_replaced maybe_token
+        else new_token_list_with_new_token_for char
         end
       end
+    end
+
+    sig { params(char: String).returns NilToken }
+    def maybe_token_for_last_lexeme_with char
+      TokenFactory::for last_lexeme_with char
+    end
+
+    sig { params(token: Token).returns TokenList }
+    def new_token_list_with_last_token_replaced token
+      TokenList::new [*tokens[..-2], token]
+    end
+
+    sig { params(char: String).returns TokenList }
+    def new_token_list_with_new_token_for char
+      TokenList::new [*tokens, T.must(TokenFactory::for(char))]
+    rescue TypeError
+      raise TypeError, "Could not create token for #{char.inspect}"
     end
 
     sig { params(char: String).returns String }
--- spec/unit/truby/token_list_spec.rb
+++ spec/unit/truby/token_list_spec.rb
@@ -1,4 +1,59 @@
 # frozen_string_literal: true
 
 describe Truby::TokenList do
+  describe '.empty' do
+    specify {
+      expect(described_class.empty).to eq t([])
+    }
+  end
+
+  describe '#==' do
+    specify {
+      expect(t([[:id, 'foo'], [:id, 'bar']])).to eq(t([[:id, 'foo'], [:id, 'bar']]))
+    }
+
+    specify {
+      expect(t([[:id, 'foo'], [:id, 'bar']])).not_to eq(t([[:id, 'foo'], [:id, 'baz']]))
+    }
+
+    specify {
+      expect(t([[:id, 'foo'], [:id, 'bar']])).not_to eq(t([[:id, 'foo'], [:id, 'baz']]))
+    }
+
+    specify {
+      expect(t([[:id, 'foo'], [:id, 'bar']])).not_to eq(t([[:id, 'foo']]))
+    }
+  end
+
+  describe '#add' do
+    specify {
+      expect(t([]).add('f')).to eq t([[:id, 'f']])
+    }
+
+    specify {
+      expect(t([[:id, 'f']]).add('o')).to eq t([[:id, 'fo']])
+    }
+
+    specify {
+      expect(t([[:id, 'f']]).add('=')).to eq t([[:id, 'f'], [:assign, '=']])
+    }
+
+    specify {
+      expect(t([[:id, 'f']]).add('=').add('f')).to eq t([[:id, 'f'], [:assign, '='], [:id, 'f']])
+    }
+  end
+
+  describe '#new_token_list_with_last_token_replaced' do
+    specify {
+      expect(t([[:id, 'foo'], [:id, 'foo'], [:id, 'foo'], [:id, 'bar']]).new_token_list_with_last_token_replaced(t(:id, 'baz'))).to(
+        eq t([[:id, 'foo'], [:id, 'foo'], [:id, 'foo'], [:id, 'baz']])
+      )
+    }
+
+    specify {
+      expect(t([[:id, 'foo'], [:id, 'foo'], [:id, 'bar']]).new_token_list_with_last_token_replaced(t(:id, 'baz'))).to(
+        eq t([[:id, 'foo'], [:id, 'foo'], [:id, 'baz']])
+      )
+    }
+  end
 end
